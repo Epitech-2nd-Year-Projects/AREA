b7dbc23fd9898420301b1df132f07e276f452f8f
Yanis897349 <146654215+Yanis897349@users.noreply.github.com>
2025-09-24 13:42:30 +0000

    feat(web/dashboard): layout

---
+++ web/src/components/dashboard/dashboard-sidebar.tsx
@@ -0,0 +1,139 @@
+'use client'
+
+import * as React from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail
+} from '@/components/ui/sidebar'
+import {
+  LinkIcon,
+  LogsIcon,
+  NotebookTextIcon,
+  ShieldUserIcon
+} from 'lucide-react'
+import { UserRole } from '@/lib/api/contracts/users'
+import { useTranslations } from 'next-intl'
+import { UserDropdown } from './user-dropdown'
+
+type NavItem = {
+  internationalizedTitle: string
+  href: string
+  icon?: React.ReactNode
+}
+
+type NavGroup = {
+  internationalizedTitle: string
+  allowedRoles?: UserRole[]
+  items: NavItem[]
+}
+
+const navData: {
+  navMain: NavGroup[]
+} = {
+  navMain: [
+    {
+      internationalizedTitle: 'admin',
+      allowedRoles: [UserRole.Admin],
+      items: [
+        {
+          internationalizedTitle: 'adminLogs',
+          href: '/dashboard/admin/logs',
+          icon: <LogsIcon />
+        },
+        {
+          internationalizedTitle: 'adminUsers',
+          href: '/dashboard/admin/users',
+          icon: <ShieldUserIcon />
+        }
+      ]
+    },
+    {
+      internationalizedTitle: 'myArea',
+      allowedRoles: [UserRole.Admin, UserRole.User],
+      items: [
+        {
+          internationalizedTitle: 'services',
+          href: '/dashboard',
+          icon: <NotebookTextIcon />
+        },
+        {
+          internationalizedTitle: 'links',
+          href: '/dashboard/links',
+          icon: <LinkIcon />
+        }
+      ]
+    }
+  ]
+}
+
+function canAccessGroup(userRole: UserRole, groupRoles?: UserRole[]) {
+  if (!groupRoles) return true
+  const groupRole = groupRoles.find((role) => role === userRole)
+  return groupRole !== undefined
+}
+
+export function DashboardSidebar(props: React.ComponentProps<typeof Sidebar>) {
+  const t = useTranslations('Sidebar')
+  const pathname = usePathname()
+
+  // TODO: Check if user is logged in, redirect accordingly
+  const user = {
+    id: '1',
+    name: 'John Doe',
+    email: 'john@doe.com',
+    imageUrl: '',
+    role: UserRole.Admin
+  }
+
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader></SidebarHeader>
+      <SidebarContent>
+        {navData.navMain
+          .filter((group) => canAccessGroup(user.role, group.allowedRoles))
+          .map((group) => (
+            <SidebarGroup key={group.internationalizedTitle}>
+              <SidebarGroupLabel>
+                {t(group.internationalizedTitle)}
+              </SidebarGroupLabel>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {group.items.map((item) => {
+                    const isActive =
+                      pathname === item.href ||
+                      pathname.startsWith(item.href + '/')
+                    return (
+                      <SidebarMenuItem key={item.internationalizedTitle}>
+                        <SidebarMenuButton asChild isActive={isActive}>
+                          <Link href={item.href}>
+                            {item.icon}
+                            {t(item.internationalizedTitle)}
+                          </Link>
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                    )
+                  })}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
+          ))}
+      </SidebarContent>
+      <SidebarFooter>
+        <UserDropdown user={user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
