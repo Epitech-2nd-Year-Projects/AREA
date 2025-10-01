3a1fea003677c414f50f7bbf1af83769eae4d341
Laurentaliu <laurent.aliu@epitech.eu>
2025-10-01 19:51:33 +0200

    chore(mobile/data/): remove comment

--- mobile/lib/features/areas/data/repositories/area_repository_impl.dart
+++ mobile/lib/features/areas/data/repositories/area_repository_impl.dart
@@ -2,7 +2,6 @@ import '../../domain/entities/area.dart';
 import '../../domain/repositories/area_repository.dart';
 
 class AreaRepositoryImpl implements AreaRepository {
-  // Liste simulant la base de données des Areas de l'utilisateur courant
   final List<Area> _areas = [
     Area(
       id: '1',
@@ -24,7 +23,6 @@ class AreaRepositoryImpl implements AreaRepository {
 
   @override
   Future<List<Area>> getAreas() async {
-    // Simuler un délai réseau
     await Future.delayed(Duration(milliseconds: 500));
     return _areas;
   }
@@ -32,7 +30,6 @@ class AreaRepositoryImpl implements AreaRepository {
   @override
   Future<Area> createArea(Area area) async {
     await Future.delayed(Duration(milliseconds: 500));
-    // Créer un nouvel objet Area avec un ID généré
     final newArea = Area(
       id: _generateId(), 
       userId: area.userId, 
