5ba6cab8c4cf76fc8746681b9df9d9b0368e5c83
Laurentaliu <laurent.aliu@epitech.eu>
2025-10-03 00:50:44 +0200

    feat(mobile/features/auth/router): add good import for profile page

--- mobile/lib/features/auth/presentation/router/auth_router.dart
+++ mobile/lib/features/auth/presentation/router/auth_router.dart
@@ -8,6 +8,7 @@ import '../../../areas/presentation/pages/area_form_page.dart';
 import '../../../areas/domain/entities/area.dart';
 import '../../../services/presentation/pages/service_details_page.dart';
 import '../../../services/presentation/pages/services_list_page.dart';
+import '../../../profile/presentation/pages/profile_page.dart';
 import '../blocs/auth_bloc.dart';
 import '../pages/login_page.dart';
 import '../pages/register_page.dart';
@@ -120,25 +121,5 @@ class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(child: Text('Dashboard Page 🚀'));
-  }
-}
-
-// class AreasPage extends StatelessWidget {
-//   const AreasPage({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Center(
-//       child: Text('Areas Page ⚡'),
-//     );
-//   }
-// }
-
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(child: Text('Profile Page 👤'));
   }
 }
