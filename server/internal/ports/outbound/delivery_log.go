b3acee656648fbd479c80d32ceab150826c8f8b0
gregorsternat <gregor.sternat@epitech.eu>
2025-10-19 17:01:39 +0200

    feat(outbound): extend repositories with detailed retrieval methods

--- server/internal/ports/outbound/delivery_log.go
+++ server/internal/ports/outbound/delivery_log.go
@@ -4,9 +4,11 @@ import (
 	"context"
 
 	jobdomain "github.com/Epitech-2nd-Year-Projects/AREA/server/internal/domain/job"
+	"github.com/google/uuid"
 )
 
 // DeliveryLogRepository persists HTTP delivery traces for executed jobs
 type DeliveryLogRepository interface {
 	Create(ctx context.Context, log jobdomain.DeliveryLog) (jobdomain.DeliveryLog, error)
+	ListByJob(ctx context.Context, jobID uuid.UUID, limit int) ([]jobdomain.DeliveryLog, error)
 }
