0dc4f71f6e9ebd495927158c596f739b13436ec3
Laurentaliu <laurent.aliu@epitech.eu>
2025-11-02 15:36:04 +0100

    feat(lib/features/areas/domain/repositories): add updateAreaStatus funciton to abstract class

--- mobile/lib/features/areas/domain/repositories/area_repository.dart
+++ mobile/lib/features/areas/domain/repositories/area_repository.dart
@@ -1,10 +1,12 @@
 import '../entities/area.dart';
 import '../entities/area_draft.dart';
+import '../entities/area_status.dart';
 
 abstract class AreaRepository {
   Future<List<Area>> getAreas();
   Future<Area> createArea(AreaDraft draft);
   Future<Area> updateArea(String areaId, AreaDraft draft);
+  Future<Area> updateAreaStatus(String areaId, AreaStatus status);
   Future<void> deleteArea(String areaId);
   Future<void> executeArea(String areaId);
 }
